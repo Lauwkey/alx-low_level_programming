dd0c437cf8f484c83eabdf5febafe2200244b4c3
Lauwkey <mlulekimongwe@gmail.com>
2023-03-31 13:54:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** main - A program that print a new line using printf function
+ * Return: 0 (success)
+ */
+int mai(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0)
+}
